4812f8d1437d1d19ba4c09bb0894c32f55aa67a5
박성진 <seongjin8247@gmail.com>
2024-05-24 23:00:57 +0900

    style: added style on login,out User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 export default function User({ user: { photoURL, displayName } }) {
   return (
-    <div>
-      <img src={photoURL} alt={displayName} />
-      <span>{displayName}</span>
+    <div className="flex items-center">
+      <img
+        className="w-10 h-10 rounded-full mr-2"
+        src={photoURL}
+        alt={displayName}
+      />
+      <span className="hidden md:block">{displayName}</span>
     </div>
   );
 }
