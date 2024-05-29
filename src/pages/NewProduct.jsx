dd56db76bef07d525e0f88e1ae3f0cf0e849430e
박성진 <seongjin8247@gmail.com>
2024-05-29 13:29:25 +0900

    feat: added form with inputs on NewProduct.jsx

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function NewProduct() {
-  return <div>NewProduct</div>;
+  return (
+    <form>
+      <h1>새로운 제품 등록</h1>
+      <input type="file" />
+      <input type="text" placeholder="제품명" />
+      <input type="number" placeholder="가격" />
+      <input type="text" placeholder="카테고리" />
+      <input type="text" placeholder="제품 설명" />
+      <input type="text" placeholder="옵션들(,콤마)로 구분" />
+      <button>제품 등록하기</button>
+    </form>
+  );
 }
